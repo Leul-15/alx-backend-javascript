b085e33a75489c6380566659eaf795935810e338
Leul <leulassefa255@gmail.com>
2024-04-08 05:26:17 -0700

    ES6 Basics

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
+  let employeeNames = '';
+
   for (const item of reportWithIterator) {
-    console.log(item);
+    employeeNames += item.employee + '|';
   }
+
+  employeeNames = employeeNames.slice(0, -1);
+
+  return employeeNames;
 }

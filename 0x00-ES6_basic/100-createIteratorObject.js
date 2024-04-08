cd35023bd4a115fa94c46c6bb01488f06438b68e
Leul <leulassefa255@gmail.com>
2024-04-08 07:25:22 -0700

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,28 @@
 export default function createIteratorObject(report) {
+  const departments = Object.keys(report.allEmployees);
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
   return {
-    [Symbol.iterator]: function* () {
-      for (const department in report.allEmployees) {
-        for (const employee of report.allEmployees[department]) {
-          yield employee;
-        }
+    next() {
+      if (currentDepartmentIndex >= departments.length) {
+        return { done: true };
       }
-    }
+
+      const currentDepartment = departments[currentDepartmentIndex];
+      const employees = report.allEmployees[currentDepartment];
+
+      if (currentEmployeeIndex >= employees.length) {
+        currentDepartmentIndex++;
+        currentEmployeeIndex = 0;
+        return this.next();
+      }
+
+      const employee = employees[currentEmployeeIndex++];
+      return { value: employee, done: false };
+    },
+    [Symbol.iterator]() {
+      return this;
+    },
   };
 }

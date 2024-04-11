fc2475f3ca744092499a04bf2c468f64b0a89692
Leul <leulassefa255@gmail.com>
2024-04-11 08:45:21 -0700

    ES6 classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,27 +1,7 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
-  }
-
-
-  displayFullPrice() {
-    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
-  }
-
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
-
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
-    this._currency = newCurrency;
   }
 
   get amount() {
@@ -30,5 +10,21 @@ export default class Pricing {
 
   get currency() {
     return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }

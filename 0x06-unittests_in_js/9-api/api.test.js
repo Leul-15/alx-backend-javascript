30212cf5b9e876f7e537716d6fa496f0db049bad
Leul <leulassefa255@gmail.com>
2024-06-06 10:38:37 -0700

    Unittests in JS

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -28,7 +28,7 @@ describe('API integration test', () => {
   });
 
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
-    request.get(`${API_URL}/cart/d221`, (_err, res, _body) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
